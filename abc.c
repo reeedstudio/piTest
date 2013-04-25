641a499ffd250b5f0db1b5afc6568e49932815de
reeedstudio <luweicong@qq.com>
2013-04-25 21:49:51 +0800

    ...

--- abc.c
+++ abc.c
@@ -2,27 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-int main (int argc,char* argv[])
+int main ()
 {
-  if (argc < 2) {
-    printf("Usage example: ./led 4 \n");
-    return 1;
-  }
-  int pinNumber = atoi(argv[1]);
+
+    int pinNumber = 14;
+    if (-1 == wiringPiSetup()) 
+    {
+        printf("Setup wiringPi failed!");
+        return 1;
+    }
  
-  if (-1 == wiringPiSetup()) {
-    printf("Setup wiringPi failed!");
-    return 1;
-  }
- 
-  pinMode(pinNumber, OUTPUT); // set mode to output 
-  while(1) {
-    digitalWrite(pinNumber, 1); // output a high level 
-    delay(800);
-    digitalWrite(pinNumber, 0); // output a low level 
-    delay(800);
-  }
+    pinMode(pinNumber, OUTPUT); // set mode to output 
+    for(int i=0; i<10; i++)
+    {
+        digitalWrite(pinNumber, 1); // output a high level 
+        delay(800);
+        digitalWrite(pinNumber, 0); // output a low level 
+        delay(800);
+    }
  
     // hello, just test
-  return 0;
+    return 0;
 }
